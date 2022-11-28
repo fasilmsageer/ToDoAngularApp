d43dee1d189303c4e1a4484cab0d5b5842ea403b
fasilmsageer <fasilmsageer@gmail.com>
2022-11-28 21:47:06 +0530

    usercred

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AddToDoComponent } from './add-to-do/add-to-do.component';
 import { ViewAllComponent } from './view-all/view-all.component';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
 
 const myRoute=[
   {
@@ -29,7 +30,8 @@ const myRoute=[
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(myRoute)
+    RouterModule.forRoot(myRoute),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
